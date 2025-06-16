ab3a29cbfc845f8f23af02af8c3716b89d2b4a86
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-16 11:47:08 -0400

    updated profile pic component

--- components/profile-pic.tsx
+++ components/profile-pic.tsx
@@ -5,16 +5,18 @@ import { ThemeToggle } from './theme-toggle';
 
 export function ProfilePicture() {
   return (
-    <div className="relative w-[400px] h-[400px]">
-      <Image
-        src="/merrick.jpg"
-        alt="Profile picture of Merrick"
-        width={400}
-        height={400}
-        className="rounded-full object-cover"
-      />
-      <div className="absolute top-1 right-0 z-10">
-        <ThemeToggle />
+    <div className="flex justify-center items-center max-w-[80%]">
+      <div className="relative w-[400px] h-[400px]">
+        <Image
+          src="/merrick.jpg"
+          alt="Profile picture of Merrick"
+          width={400}
+          height={400}
+          className="rounded-full object-cover"
+        />
+        <div className="absolute top-1 right-1 z-10">
+          <ThemeToggle />
+        </div>
       </div>
     </div>
   );
