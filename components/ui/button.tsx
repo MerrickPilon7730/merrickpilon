bb34e207da6133077100c01a511fa5d4aa02bce6
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-16 14:09:37 -0400

    edited the shadcn button variants

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -22,9 +22,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        default: "h-9 px-4 w-auto py-2 has-[>svg]:px-3",
+        sm: "h-8 rounded-md w-auto gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-md w-auto px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
