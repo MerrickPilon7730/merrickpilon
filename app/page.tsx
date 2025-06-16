d61d084093585758586cc113321fc37905ef4326
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-16 11:47:34 -0400

    Created introduction component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 
+import { Introduction } from "@/components/introduction";
 import { ProfilePicture } from "@/components/profile-pic";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-2">
-      <div>
-        <ProfilePicture />
-      </div>
-
+    <div className="grid grid-cols-2 mt-30">
+      <ProfilePicture />
+      <Introduction />
     </div>
   );
 }
---
+++ components/introduction.tsx
@@ -0,0 +1,10 @@
+
+
+
+export function Introduction() {
+    return(
+        <div className="flex justify-start items-start">
+            <h1 className="text-4xl font-bold">MERRICK PILON</h1>
+        </div>
+    )
+}
