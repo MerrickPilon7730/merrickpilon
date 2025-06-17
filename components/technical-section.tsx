99c4bd4a591bbfd1eddc6f3e2c532a8ac17ff237
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-17 09:59:48 -0400

    added frameworks component to my main page

--- components/technical-section.tsx
+++ components/technical-section.tsx
@@ -1,9 +1,11 @@
+import { Frameworks } from "./frameworks";
 import { Languages } from "./languages";
 
 export function Technical() {
     return(
-        <div className="mx-auto ">
+        <div className="mx-auto max-w-[85%]">
             <Languages />
+            <Frameworks />
         </div>
     );
 };
