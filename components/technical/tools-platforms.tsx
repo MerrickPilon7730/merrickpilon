e39196dbdbdf7f32e5aad8344ce0180eda833027
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-20 09:43:58 -0400

    Fixed formatting bug

--- components/technical/tools-platforms.tsx
+++ components/technical/tools-platforms.tsx
@@ -16,7 +16,7 @@ export function Tools() {
             <h1 className="font-bold flex items-center justify-center text-3xl">TOOLS & PLATFORMS</h1>
             <div className="flex flex-wrap justify-center gap-4 mt-4 font-mono">
                 {tools.map((tool, index) => (
-                <p key={index} className="w-[12%] min-w-[100px] text-center">
+                <p key={index} className="w-[13%] min-w-[100px] text-center">
                     {tool}
                 </p>
                 ))}
