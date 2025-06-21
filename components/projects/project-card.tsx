280a2ac11b7e55165bc9461f61f7a2bd46042992
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-21 11:00:54 -0400

    added onClick function to project card to open modal

--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image';
 import { ProjectType } from '@/lib/projects';
 
 type props = {
+  onClick: () => void;
   project: ProjectType;
 }
 
-export function ProjectCard({ project }: props) {
+export function ProjectCard({ onClick, project }: props) {
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-2xl shadow-md overflow-hidden">
+    <div className="bg-white dark:bg-gray-800 rounded-2xl shadow-md overflow-hidden hover:cursor-pointer" onClick={onClick}>
       <div className="relative w-full h-60">
         <Image
           src={project.displayImage}
