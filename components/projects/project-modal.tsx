d7c11fcb83c0a9535dcd597a4fc7335a6af303eb
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-21 19:51:25 -0400

    used project header and project images component inside the project modal component

--- components/projects/project-modal.tsx
+++ components/projects/project-modal.tsx
@@ -8,6 +8,8 @@ import {
 } from "@/components/ui/card";
 
 import { ProjectType } from "@/lib/projects";
+import { ProjectHeader } from "./project-header";
+import { ProjectImages } from "./project-images";
 
 type Props ={
     isOpen: boolean;
@@ -23,11 +25,16 @@ export function ProjectModal({isOpen, onClose, project}: Props) {
             className="fixed inset-0 z-50 bg-black/60 flex items-center justify-center overflow-auto"
             onClick={onClose}
         >
-            <div className="w-[90%] max-w-4xl"  onClick={(e) => e.stopPropagation()}>
+            <div className="w-[90%] max-w-[70%]"  onClick={(e) => e.stopPropagation()}>
                 <Card className="bg-white dark:bg-gray-800 dark:text-white">
                     <CardHeader>
-                        <CardTitle>
-                            {project?.title}
+                        <CardTitle className="flex grid-cols-1 lg:grid-cols-2 gap-x-2">
+                            <div className="order-1 lg:order-2 max-w-[50%] w-[45%]">
+                                <ProjectHeader project={project}/>
+                            </div>
+                            <div className="order-2 lg:order-1 max-w-50% w-[45%]">
+                                <ProjectImages project={project}/>
+                            </div>
                         </CardTitle>
                     </CardHeader>
                     <CardContent >
