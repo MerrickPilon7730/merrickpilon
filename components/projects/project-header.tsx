89da8801eb85ec176aed53cfe197dd26118443e0
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-23 15:15:01 -0400

    updated project-header to use technologies schema

--- components/projects/project-header.tsx
+++ components/projects/project-header.tsx
@@ -8,17 +8,45 @@ type Props = {
 export function ProjectHeader({project}: Props) {
     return(
         <div className="flex flex-col items-center justify-center">
-                <p>{project?.title}</p>
-                <div>
-                    <a
-                    href={`https://${project?.url}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-400 hover:underline"
-                    >
-                    {project?.url}
-                    </a>
+            <p className="text-2xl font-bold">{project?.title}</p>
+            <div className="mt-2">
+                <a
+                href={`https://${project?.url}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 hover:underline"
+                >
+                {project?.url}
+                </a>
+            </div>
+
+            <div className="w-[80%] mt-4">
+                <div className="flex gap-4">
+                    <p className="font-bold w-[120px] text-left">Back-end:</p>
+                    <p className="font-mono flex-1 justify-left">{project?.technologies.languages}</p>
                 </div>
+            </div>
+
+            <div className="w-[80%] mt-4">
+                <div className="flex gap-4">
+                    <p className="font-bold w-[120px] text-left">Front-end:</p>
+                    <p className="font-mono flex-1 justify-left">{project?.technologies.frontend}</p>
+                </div>
+            </div>
+
+            <div className="w-[80%] mt-4">
+                <div className="flex gap-4">
+                    <p className="font-bold w-[120px] text-left">Back-end:</p>
+                    <p className="font-mono flex-1 justify-left">{project?.technologies.backend}</p>
+                </div>
+            </div>
+
+            <div className="w-[80%] mt-4">
+                <div className="flex gap-4">
+                    <p className="font-bold w-[120px] text-left">Tools:</p>
+                    <p className="font-mono flex-1 justify-left">{project?.technologies.tools}</p>
+                </div>
+            </div>
         </div>
     )
 }
