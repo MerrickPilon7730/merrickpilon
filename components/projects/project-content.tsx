02cc3fe3bddec4417b0d0232f6df7733396b3ceb
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-06-27 13:43:53 -0400

    Content is now dynamic for all screen sizes

--- components/projects/project-content.tsx
+++ components/projects/project-content.tsx
@@ -7,23 +7,20 @@ type Props = {
 
 export function ProjectContent({project}: Props) {
     return(
-        <div className="w-[80%]">
-            <div className="flex gap-4">
-                <p className="font-bold w-[120px] text-left">Description:</p>
+        <div className="w-full max-w-4xl px-4">
+            <div className="flex flex-col sm:flex-row gap-2 sm:gap-4 mb-4">
+                <p className="font-bold sm:w-[120px] text-left">Description:</p>
                 <p className="font-mono flex-1 text-left">{project?.content}</p>
             </div>
 
-            <div className="flex gap-4 mt-4">
-                <p className="font-bold w-[120px] text-left">Features:</p>
-                <p className="font-mono flex-1 text-left mb-2">
+            <div className="flex flex-col sm:flex-row gap-2 sm:gap-4">
+                <p className="font-bold sm:w-[120px] text-left">Features:</p>
+                <div className="font-mono flex-1 text-left space-y-1">
                     {project?.features.map((feature, index) => (
-                        <span key={index}>
-                        {feature}
-                        <br />
-                        </span>
+                        <p key={index}>{feature}</p>
                     ))}
-                </p>
+                </div>
             </div>
         </div>
     );
-};
+}
