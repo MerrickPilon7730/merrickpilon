9f0fd0cc6a961bea49d38fd5ec8353195f6a43de
MerrickPilon7730 <144726859+MerrickPilon7730@users.noreply.github.com>
2025-08-07 11:41:43 -0400

    Added a redirect

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,15 @@
 
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-center p-6">
       <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
       <p className="text-lg">
-        This page does not exist
+        This page does not exist, go to{" "}
+        <Link href="/" className="text-blue-500 hover:underline">
+            Home
+        </Link>
       </p>
     </div>
   );
